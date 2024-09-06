597fd9d70a8c27f70249e584bec59ffcd927c259
MugishBeldar <mugishbeldar333@gmail.com>
2024-09-06 15:19:47 +0530

    Test.js deleted

--- test.js
+++
@@ -1,7 +0,0 @@
-
-
-const data = `2024-03-24 02:10:35 - ServiceNow API (Work notes)\nWork note copied from Parent Incident: [xMatters] - xM event ID 55549000 has an updated status of terminated\n\n2024-03-23 23:10:34 - ServiceNow API (Work notes)\nWork note copied from Parent Incident: [xMatters] - xM event ID 55549000 has an updated status of active\n\n`;
-
-const parsedData = parseNotes(data);
-console.log(parsedData);
-
